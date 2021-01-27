9adfcce6475cac059b822c882366d115bc3f8e18
migueljcastro <migueljcastro03@gmail.com>
2021-01-26 20:52:09 -0500

    Sun added but with errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import sun.security.provider.Sun;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,6 +10,12 @@ public class Main {
         FactorX.Sun=1;
         FactorX.Moon=3;
         FactorX.Planets=2;
+
+
+        SUN Freon = new SUN();
+        Freon.color="500kj";
+        Freon.size="7000km";
+        Freon.Temp="40000c";
 
     }
 }
